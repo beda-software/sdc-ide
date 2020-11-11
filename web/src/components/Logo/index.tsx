eaa3d42df1810cf6930de59df7972ffead9573a6
Alexander Pavlushkin <pavlushkin87@gmail.com>
2020-11-11 12:19:55 +0700

    Add styles for the demo page

---
+++ web/src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import s from './Logo.module.scss';
+
+export function Logo() {
+    return (
+        <a className={s.logo} href="https://beda.software" target="_blank" rel="noopener noreferrer">
+            <span />
+        </a>
+    );
+}
--- web/src/containers/DemoPage/index.tsx
+++ web/src/containers/DemoPage/index.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import s from './DemoPage.module.scss';
+
 import { Button } from 'src/components/Button';
+import { Logo } from 'src/components/Logo';
 
 export function DemoPage() {
     return (
-        <div className={s.container}>
-            <header className={s.header} />
-            <div className={s.patientFHIRResourceBox}>patientFHIRResourceBox</div>
-            <div className={s.questFHIRResourceBox}>questFHIRResourceBox</div>
-            <div className={s.patientBatchRequestBox}>patientBatchRequestBox</div>
-            <div className={s.patientFormBox}>
-                patientFormBox
-                <Button variant="primary">Save</Button>
+        <div className={s.mainContainer}>
+            <div className={s.upperRowContainer}>
+                <div className={s.patientFHIRResourceBox}>
+                    <h2>Patient FHIR Resource</h2>
+                </div>
+                <div className={s.questFHIRResourceBox}>
+                    <h2>Questionanire FHIR Resource</h2>
+                </div>
+                <div className={s.patientFormBox}>
+                    <h2>Patient Form</h2>
+                    <Button variant="primary">Save</Button>
+                </div>
+                <div className={s.patientBatchRequestBox}>
+                    <h2>Patient batch request</h2>
+                </div>
             </div>
-            <div className={s.patientMapperBox}>patientMapperBox</div>
+
+            <div className={s.lowerRowContainer}>
+                <div className={s.patientMapperBox}>
+                    <h2>Patient JUTE Mapping</h2>
+                </div>
+            </div>
+            <Logo />
         </div>
     );
 }
