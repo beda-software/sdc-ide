44055da116bab802b2e9e46fb2507e3323387afe
Alexander Pavlushkin <pavlushkin87@gmail.com>
2020-11-23 11:51:31 +0700

    Remove console.log

--- web/src/utils/mapping.ts
+++ web/src/utils/mapping.ts
@@ -2,10 +2,9 @@ import { QuestionnaireResponse } from 'shared/lib/contrib/aidbox';
 import { service } from 'aidbox-react/lib/services/service';
 
 export const applyMapping = async (mappingId: string, questionnaireResponse: QuestionnaireResponse) => {
-    const response = await service({
+    await service({
         method: 'POST',
         url: `/Mapping/${mappingId}/$apply`,
         data: questionnaireResponse,
     });
-    console.log('applyMapping response', response);
 };
