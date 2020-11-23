9eb05ac49ec8b7464b87f3497591868ce42a258c
Alexander Pavlushkin <pavlushkin87@gmail.com>
2020-11-23 11:57:28 +0700

    Remove old DemoPage components

--- web/src/containers/App/index.tsx
+++ web/src/containers/App/index.tsx
@@ -3,16 +3,12 @@ import { Route, Switch, Router, Redirect } from 'react-router-dom';
 
 import { history } from 'src/services/history';
 
-import { DemoPage } from 'src/containers/DemoPage';
 import { Main } from 'src/containers/Main';
 
 export function App() {
     return (
         <Router history={history}>
             <Switch>
-                <Route path="/old/:id" exact>
-                    <DemoPage />
-                </Route>
                 <Route path="/:questionnaireId" exact>
                     <Main />
                 </Route>
