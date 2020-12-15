c9f2115597b7bb80112c424c161a7570b188718c
Alexander Pavlushkin <pavlushkin87@gmail.com>
2020-12-16 03:12:09 +0700

    Remove unused sample resources

--- web/src/containers/Main/__test__/main-callback.test.ts
+++ web/src/containers/Main/__test__/main-callback.test.ts
@@ -12,9 +12,7 @@ import mappingTest1 from './resources/Mapping/test-1.json';
 import mappingTest1New from './resources/Mapping/test-1-new.json';
 import mappingTest2 from './resources/Mapping/test-2.json';
 import questionnaireTest1 from './resources/Questionnaire/test-1.json';
-import questionnaireTest1FHIRUpdated from './resources/Questionnaire/test-1-fhir-updated.json';
-import questionnaireTest2 from './resources/Questionnaire/test-2.json';
-import questionnaireTest3 from './resources/Questionnaire/test-3.json';
+import questionnaireTest1FHIRNew from './resources/Questionnaire/test-1-fhir-new.json';
 import questionnaireResponseNew from './resources/QuestionnaireResponse/demo-1-new.json';
 
 async function setup() {
@@ -22,14 +20,7 @@ async function setup() {
         service({
             method: 'PUT',
             url: '',
-            data: [
-                patientTest1,
-                mappingTest1,
-                mappingTest2,
-                questionnaireTest1,
-                questionnaireTest2,
-                questionnaireTest3,
-            ],
+            data: [patientTest1, mappingTest1, mappingTest2, questionnaireTest1],
         }),
     );
     return response;
@@ -44,7 +35,7 @@ test('saveQuestionnaireFHIR', async () => {
             ensure(result.current.questionnaireRD);
         });
         await act(async () => {
-            await result.current.saveQuestionnaireFHIR(questionnaireTest1FHIRUpdated as Questionnaire);
+            await result.current.saveQuestionnaireFHIR(questionnaireTest1FHIRNew as Questionnaire);
         });
         await waitFor(() => {
             const qUpdated = ensure(result.current.questionnaireRD);
