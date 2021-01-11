4d99f2ab39cf330b4561de83343cad37fa069999
Dana Vergilyush <dvergilyush@gmail.com>
2021-01-12 02:55:40 +0300

    Update ResourceSelect. Add display prop

--- web/src/components/ResourceSelect/index.tsx
+++ web/src/components/ResourceSelect/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import { isSuccess, RemoteData } from 'aidbox-react/lib/libs/remoteData';
 import { AidboxResource, Bundle } from 'shared/lib/contrib/aidbox';
 
+import s from './ResourceSelect.module.scss';
+
 interface ResourceSelectProps<R> {
+    cssClass: string;
     value: string;
     bundleResponse: RemoteData<Bundle<R>>;
     onChange: (resourceId: string) => void;
+    display?: (resource: R) => string;
 }
 
-export function ResourceSelect<R extends AidboxResource>({ value, bundleResponse, onChange }: ResourceSelectProps<R>) {
+export function ResourceSelect<R extends AidboxResource>({
+    cssClass,
+    value,
+    bundleResponse,
+    onChange,
+    display,
+}: ResourceSelectProps<R>) {
     if (!isSuccess(bundleResponse)) {
         return null;
     }
@@ -16,14 +28,16 @@ export function ResourceSelect<R extends AidboxResource>({ value, bundleResponse
     const entries = bundleResponse.data.entry || [];
 
     return (
-        <div>
-            <select value={value} onChange={(e) => onChange(e.target.value)}>
-                {entries.map((entry, key) => (
-                    <option key={key} value={entry.resource!.id}>
-                        {entry.resource!.id}
-                    </option>
-                ))}
-            </select>
-        </div>
+        <select
+            className={classNames(s.resourceSelect, cssClass)}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+        >
+            {entries.map((entry, key) => (
+                <option key={key} value={entry.resource!.id}>
+                    {display ? display(entry.resource!) : entry.resource!.id}
+                </option>
+            ))}
+        </select>
     );
 }
