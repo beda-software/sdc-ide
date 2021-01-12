4ca06df75cb7858ea5ddb2e60e5b19f046ca1a36
Dana Vergilyush <dvergilyush@gmail.com>
2021-01-12 03:06:27 +0300

    Update ExpandableElement

--- web/src/components/ExpandableElement/index.tsx
+++ web/src/components/ExpandableElement/index.tsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import s from './ExpandableElement.module.scss';
 
 interface ExpandableElementProps {
     cssClass: string;
-    title: string;
+    title: string | React.ReactElement;
     children: React.ReactElement;
 }
 
 export function ExpandableElement(props: ExpandableElementProps) {
     const [expanded, setExpanded] = useState(false);
+    const headerRef = useRef(null);
     return (
         <div className={props.cssClass} style={expanded ? { flex: 4 } : {}}>
-            <h2 className={s.title} onClick={() => setExpanded((f) => !f)}>
+            <h2
+                className={s.title}
+                ref={headerRef}
+                onClick={(e) => {
+                    if (e.target === headerRef.current) {
+                        setExpanded((f) => !f);
+                    }
+                }}
+            >
                 {props.title}
             </h2>
             {props.children}
