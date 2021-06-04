d2a44386755f8e06beb4c83409fe9069f3130ae5
Ilya Beda <ir4y.ix@gmail.com>
2021-06-04 15:37:03 +0300

    Update samples data

--- dump-local-samples.sh
+++ dump-local-samples.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "" > demo-data.ndjson
 
-curl -H 'accept: application/json' -u root:secret 'http://localhost:8080/Patient/$dump' >> demo-data.ndjson
-curl -H 'accept: application/json' -u root:secret 'http://localhost:8080/Questionnaire/$dump' >> demo-data.ndjson
-curl -H 'accept: application/json' -u root:secret 'http://localhost:8080/Mapping/$dump' >> demo-data.ndjson
+curl -H 'accept: application/json' -u root:secret 'http://localhost:9000/Patient/$dump' >> demo-data.ndjson
+curl -H 'accept: application/json' -u root:secret 'http://localhost:9000/Questionnaire/$dump' >> demo-data.ndjson
+curl -H 'accept: application/json' -u root:secret 'http://localhost:9000/Mapping/$dump' >> demo-data.ndjson
+curl -H 'accept: application/json' -u root:secret 'http://localhost:9000/AidboxQuery/$dump' >> demo-data.ndjson
+curl -H 'accept: application/json' -u root:secret 'http://localhost:9000/ValueSet/$dump' >> demo-data.ndjson
 
 gzip demo-data.ndjson
 mv demo-data.ndjson.gz  web/public/demo-data.ndjson.gz
