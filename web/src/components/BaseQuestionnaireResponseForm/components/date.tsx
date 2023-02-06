5c3730a8f4bc4221c872e673b5233dc06e042271
Dmitry Shutov <dmitry.s@beda.software>
2023-02-06 11:53:53 +0700

    Add parse and format logic to dayTime field

--- web/src/components/BaseQuestionnaireResponseForm/components/date.tsx
+++ web/src/components/BaseQuestionnaireResponseForm/components/date.tsx
@@ -1,7 +1,8 @@
-import { Field } from "react-final-form";
-import { QuestionItemProps, useQuestionnaireResponseFormContext } from "sdc-qrf/src";
+import moment from 'moment';
+import { Field } from 'react-final-form';
+import { QuestionItemProps, useQuestionnaireResponseFormContext } from 'sdc-qrf/src';
 
-
+import { FHIRDateTimeFormat } from 'aidbox-react/lib/utils/date';
 
 export function QuestionDate({ parentPath, questionItem }: QuestionItemProps) {
     const qrfContext = useQuestionnaireResponseFormContext();
@@ -31,9 +32,16 @@ export function QuestionDateTime({ parentPath, questionItem }: QuestionItemProps
     const { linkId, text, readOnly, hidden } = questionItem;
     const fieldPath = [...parentPath, linkId, 0, 'value', 'dateTime'];
     const fieldName = fieldPath.join('.');
-
+    const parseValue = (value: string) => {
+        return moment(value).local().utc().format(FHIRDateTimeFormat);
+    };
+    const formatValue = (value?: string) => {
+        return value
+            ? moment.utc(value, FHIRDateTimeFormat).local().format('YYYY-MM-DDTHH:mm:ss')
+            : '';
+    };
     return (
-        <Field name={fieldName}>
+        <Field name={fieldName} parse={parseValue} format={formatValue}>
             {({ input, meta }) => (
                 <div>
                     <label>{text}</label>
