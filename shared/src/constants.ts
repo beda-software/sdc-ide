12a1fdd05a438043144f950640a604c0fb9093ef
Elena Grednikova <vesnushkalena@gmail.com>
2023-08-17 11:33:15 +0200

    Fix constants config

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -1,2 +1,8 @@
-export const baseURL = (window as any).BASE_URL ?? 'http://localhost:8080/';
-export const juteURL = (window as any).JUTE_URL ?? 'http://localhost:8090/'
+export const baseURL =
+    (window as any).BASE_URL === '{{BASE_URL}}'
+        ? 'http://localhost:8080/'
+        : (window as any).BASE_URL;
+export const juteURL =
+    (window as any).JUTE_URL === '{{JUTE_URL}}'
+        ? 'http://localhost:8090/'
+        : (window as any).JUTE_URL;
