4d05d7164cb13eaa892c2024bc8c99c0f1e2b96b
Ilya Beda <ilyabeda@Ilyas-MBP.ns.bigpond.net.au>
2024-06-11 15:35:36 +1000

    Change jute default port

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -4,7 +4,7 @@ export const baseURL =
         : (window as any).BASE_URL;
 export const juteURL =
     (window as any).JUTE_URL === '{{JUTE_URL}}'
-        ? 'http://localhost:8090/'
+        ? 'http://localhost:8099/'
         : (window as any).JUTE_URL;
 
 export const aiQuestionnaireBuilderUrl = 'https://builder.emr.beda.software';
