3c817f9b22d6e0ee966fd26e2c145e0b2808755c
Dmitry Shutov <dmitry.s@beda.software>
2025-05-26 21:20:23 +0500

    Fix mapping extension URL

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -22,4 +22,4 @@ export const questionnaireProfileUrl =
 export const legacyMappingExtensionUrl =
     'http://beda.software/fhir-extensions/questionnaire-mapper';
 export const mappingExtensionUrl =
-    'https://emr-core.beda.software/StructureDefinition/fhir-emr-questionnaire';
+    'https://emr-core.beda.software/StructureDefinition/questionnaire-mapper';
